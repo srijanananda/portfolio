badb8cca53db993916073542f75bbd1311b22252
srijan_ananda <srijanjas78@gmail.com>
2025-06-13 17:21:27 +0530

    Prepare for Render deployment 1

---
+++ adminpanel/management/commands/create_admin.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = "Creates a default superuser"
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("srija", "srijan1ga21is164@gmail.com", "7795260651jas")
+            self.stdout.write(self.style.SUCCESS("Superuser created"))
+        else:
+            self.stdout.write(self.style.WARNING("Superuser already exists"))
--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%03nxzbnp#^dp+)jv57mp&dp3w$rwi6_*(4f7nxmoui0phl$y@"
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: v.split(','))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -130,3 +129,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 #TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# WhiteNoise Middleware
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

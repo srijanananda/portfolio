016f36a02ba2c3d4ab5f1405f9fd7aea6bce7532
srijan_ananda <srijanjas78@gmail.com>
2025-06-13 17:34:16 +0530

    Prepare for Render deployment 3

--- adminpanel/management/commands/create_admin.py
+++ adminpanel/management/commands/create_admin.py
@@ -1,13 +1,12 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 class Command(BaseCommand):
-    help = "Creates a default superuser"
+    help = 'Creates a default admin user'
 
-    def handle(self, *args, **kwargs):
-        User = get_user_model()
-        if not User.objects.filter(username="admin").exists():
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="srija").exists():
             User.objects.create_superuser("srija", "srijan1ga21is164@gmail.com", "7795260651jas")
-            self.stdout.write(self.style.SUCCESS("Superuser created"))
+            self.stdout.write(self.style.SUCCESS('Admin user created.'))
         else:
-            self.stdout.write(self.style.WARNING("Superuser already exists"))
+            self.stdout.write(self.style.WARNING('Admin user already exists.'))
